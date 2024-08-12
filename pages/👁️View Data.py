851b01dbd0b66b5f02b22c3f25875d4f53e773fa
Victor Osei Duah <oseivictor10@yahoo.com>
2024-08-12 15:08:24 +0000

    repo update

--- pages/👁️View Data.py
+++ pages/👁️View Data.py
@@ -1,40 +1,6 @@
-# import streamlit as st
-# import pandas as pd
-# import pyodbc
-
-
-# st.title("👁️Welcome to the Data page")
-# st.write("Here you can view the data. Add your data viewing functionality here.")
-
-
-# # Connect to SQL Server
-# conn = pyodbc.connect(
-#     'DRIVER={ODBC Driver 17 for SQL Server};'
-#     'SERVER=dap-projects-database.database.windows.net;'
-#     'DATABASE=dapDB;'
-#     'UID=LP2_project;'
-#     'PWD=Stat$AndD@t@Rul3;'
-# )
-
-# # Query the data
-# query = "SELECT * FROM dbo.LP2_Telco_churn_first_3000"
-# df = pd.read_sql(query, conn)
-
-# # Display data
-# st.expander("View Full Dataset")
-# st.write(df)
-
-# # Basic data info
-# st.subheader("Data Information")
-# st.write(f"Total rows: {df.shape[0]}")
-# st.write(f"Total columns: {df.shape[1]}")
-
-# # Close the connection
-# conn.close()
-
 import streamlit as st
 import pandas as pd
-import pyodbc
+import pyodbc # type: ignore
 
 def create_connection():
     """Create a connection to the SQL Server"""
