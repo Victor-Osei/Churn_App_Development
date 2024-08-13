e4ad7951d89e9bdd68367e5a606f2031eb891af4
Victor Osei Duah <oseivictor10@yahoo.com>
2024-08-13 18:59:57 +0000

    repo update

--- pages/🏡Home Page.py
+++ pages/🏡Home Page.py
@@ -18,9 +18,7 @@ st.subheader("Key Features")
 st.markdown("""
 - Read data from SQL Server
 - Select the features for classification
-- From the dropdown choose a machine learning model of your choice
 - This app also provide a comprehensive report on the performance of the model
-- Metrics like accuracy score, precision, recall and f1-score are included in the report 
 - The app provides interactive charts for feature importance and churn probability
 """)
 
@@ -28,13 +26,8 @@ st.markdown("""
 # App features
 st.subheader("App Features")
 st.markdown("""
-1. **View Data**: Access proprietary data
             
-2. **Dashboard**: Explore interactive visuals for insights
-            
-3. **Prediction Page**: Shows the result using the machine learning model of whether a customer churned or not
-            
-4. **History Page**: 
+
 """)
 
 
