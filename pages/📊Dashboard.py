6bc1c5584971fd7f5199147fe73049ff362415ab
Victor Osei Duah <oseivictor10@yahoo.com>
2024-08-27 13:18:33 +0000

    update repo

--- pages/📊Dashboard.py
+++ pages/📊Dashboard.py
@@ -1,16 +1,3 @@
-# import streamlit as st
-
-
-# # Set page configuration
-# st.set_page_config(
-#     page_title="Dashboard",
-#     page_icon="📊", 
-#     layout="wide",
-# )
-
-# st.title("📊 Welcome to My Dashboard!")
-# st.write("This is the Dashboard. You can add your data visualizations here.")
-
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
